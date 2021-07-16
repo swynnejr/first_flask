5d2a81702c5663cd719d185b50d560048e9abfa3
swynnejr <saucersam@gmail.com>
2021-07-15 22:23:35 -0500

    robust demo

--- friend.py
+++ friend.py
@@ -9,16 +9,21 @@ class Friend:
         self.occupation = data['occupation']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+# Now we use class methods to query our database
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM friends;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
+# make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('first_flask').query_db(query)
-        # Create an empty list to append our instances of friends
+# Create an empty list to append our instances of friends
         friends = []
-        # Iterate over the db results and create instances of friends with cls.
+# Iterate over the db results and create instances of friends with cls.
         for friend in results:
             friends.append( cls(friend) )
         return friends
-            
+
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('first_flask').query_db( query, data )
